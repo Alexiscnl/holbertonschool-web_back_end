4807fc84cf3dddad49816f0c895877616e5bb92e
Alexiscnl <alexis.cornillon@gmail.com>
2025-09-01 11:47:34 +0200

    fix: remove premature process exit in stdin handling

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -4,7 +4,6 @@ process.stdin.setEncoding('utf8');
 process.stdin.on('data', (data) => {
   const name = data.trim();
   console.log(`Your name is: ${name}`);
-  process.exit();
 });
 
 process.on('exit', () => {
