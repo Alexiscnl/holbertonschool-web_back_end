d3b521d8c4ac7048baf227cf5b075caf5fb8e737
Alexiscnl <alexis.cornillon@gmail.com>
2025-09-01 11:38:06 +0200

    style: standardize string quotes and format code in 1-stdin.js

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,12 +1,12 @@
-console.log("Welcome to Holberton School, what is your name?");
+console.log('Welcome to Holberton School, what is your name?');
 
 process.stdin.setEncoding('utf8');
 process.stdin.on('data', (data) => {
-	const name = data.trim();
-	console.log(`Your name is: ${name}`);
-	process.exit();
-})
+  const name = data.trim();
+  console.log(`Your name is: ${name}`);
+  process.exit();
+});
 
-process.on('exit',() => {
-	console.log('This important software is now closing');
-})
+process.on('exit', () => {
+  console.log('This important software is now closing');
+});
