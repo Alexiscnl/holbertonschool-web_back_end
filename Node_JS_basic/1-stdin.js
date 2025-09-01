ddfd54c295cd4d46c6f604765f8a63d9b390570b
Alexiscnl <alexis.cornillon@gmail.com>
2025-09-01 11:49:43 +0200

    fix: ensure process exits after displaying user name

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -4,6 +4,7 @@ process.stdin.setEncoding('utf8');
 process.stdin.on('data', (data) => {
   const name = data.trim();
   console.log(`Your name is: ${name}`);
+  process.exit();
 });
 
 process.on('exit', () => {
